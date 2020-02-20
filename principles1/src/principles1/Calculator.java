bc6d06c3e7b40aa6beae358f37aed0bfe3cce42d
usharamya412 <39006513+usharamya412@users.noreply.github.com>
2020-02-20 15:38:54 +0530

    Add files via upload

---
+++ principles1/src/principles1/Calculator.java
@@ -0,0 +1,17 @@
+package principles1;
+
+public class Calculator {
+	public int  addition(int a,int b){
+		return a+b;
+	}
+	public int  subtract(int a,int b){
+		return a-b;
+	}
+	public int  multiply(int a,int b){
+		return a*b;
+	}
+	public int  divide(int a,int b){
+		return a/b;
+	}
+
+}
